ca5b03ca43ef8803311e2241fbf48bce88ce678c
Evgeniy-Sub8way <sub8way@gmail.com>
2026-04-20 15:48:57 +0300

    2try

--- src/task-4.ts
+++ src/task-4.ts
@@ -1,4 +1,4 @@
-function printUserInfo(name: string, age: number, email?: string) {
+function printUserInfo(name: string, age: number, email?: string): void {
   console.log('Name:', name);
   console.log('Age:', age);
   if (email) {
--- src/task-5.ts
+++ src/task-5.ts
@@ -1,6 +1,6 @@
 type Status = 'loading' | 'success' | 'error';
 
-function logStatus(status: Status) {
+function logStatus(status: Status): void {
   if (status === 'loading') {
     console.log('Loading...');
   } else if (status === 'success') {
